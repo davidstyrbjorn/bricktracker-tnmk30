f465d0ceb229a8f6525cd9f0de2afc82e6e5d8aa
Linus <itslinnet@gmail.com>
2019-01-15 14:25:41 +0100

    no script

--- site/terms.php
+++ site/terms.php
@@ -8,7 +8,6 @@
   		<meta name="author" content="Emil Bertholdsson, David Styrbjörn, Linus Karlsson, Max Benecke">
         <link href="https://fonts.googleapis.com/css?family=Quicksand|Varela+Round|Roboto|Montserrat" rel="stylesheet">
   		<link  href="../css/style.css" rel="stylesheet"/>
-  		<script src=""></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
           
         <?php
